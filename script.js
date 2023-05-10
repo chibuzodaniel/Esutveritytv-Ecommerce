bfe37d87d6461fb4699897d793c2f0719955c1d6
chibuzodaniel <chibuzodaniel71@gmail.com>
2023-05-10 11:10:51 +0100

    update

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 0); 
 })
+
+
+
+let menu = document.querySelector("#menu-icon");
+
+let navMenu = document.querySelector("#nav-menu");
+
+menu.onclick = () =>{
+    menu.classList.toggle("bx-x");
+    navMenu.classList.toggle(open);
+}
